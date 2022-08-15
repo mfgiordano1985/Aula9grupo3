a73de8c36b4e79e56d16f241208d32879000c92e
Walter Garzon <106358242+WalterGarzon@users.noreply.github.com>
2022-08-14 23:16:42 -0300

    Script SQL
    Subo script SQL

---
+++ Script.sql
@@ -0,0 +1,18 @@
+create database perros;
+use perros;
+create table duenno(
+DNI int not null auto_increment primary key,
+nombre varchar (100) not null,
+apellido varchar (80) not null,
+telefono varchar (80),
+direccion varchar(150) not null
+);
+create table perro(
+id_perro int not null auto_increment primary key,
+nombre varchar (100) not null,
+fecha_nac varchar(100),
+sexo varchar(50),
+dni_duenno int not null,
+foreign key (dni_duenno) references duenno(DNI)
+);
+INSERT INTO perro(nombre,fecha_nac,sexo,dni_duenno) VALUES
